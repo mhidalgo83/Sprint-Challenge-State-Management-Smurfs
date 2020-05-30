8dacf3dbe63be055f3c5ea528819362b63446648
Matthew Hidalgo <mhidalgo83@outlook.com>
2020-05-30 10:06:23 -0700

    able to add smurf to village

--- smurfs/src/actions/index.js
+++ smurfs/src/actions/index.js
@@ -4,10 +4,20 @@ export const FETCH_SMURFS_START = "FETCH_SMURFS_START";
 export const FETCH_SMURFS_SUCCESS = "FETCH_SMURFS_SUCCESS";
 export const FETCH_SMURFS_FAILURE = "FETCH_SMURFS_FAILURE";
 
+export const SUBMIT_SMURF_START = "SUBMIT_SMURF_START";
+export const SUBMIT_SMURF_SUCCESS = "SUBMIT_SMURF_SUCCESS";
+export const SUBMIT_SMURF_FAILURE = "SUBMIT_SMURF_FAILURE";
+
 export const getSmurfs = () => (dispatch) => {
   dispatch({ type: FETCH_SMURFS_START });
   axios
     .get("http://localhost:3333/smurfs")
     .then((res) => dispatch({ type: FETCH_SMURFS_SUCCESS, payload: res.data }))
     .catch((err) => dispatch({ type: FETCH_SMURFS_FAILURE, payload: err }));
+};
+
+export const addSmurf = (smurf) => (dispatch) => {
+  axios
+    .post("http://localhost:3333/smurfs", smurf)
+    .then((res) => console.log(res));
 };
--- smurfs/src/components/Smurfs.js
+++ smurfs/src/components/Smurfs.js
@@ -1,22 +1,68 @@
-import React, { useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { getSmurfs } from "../actions";
+import { getSmurfs, addSmurf } from "../actions";
 import Smurf from "./Smurf";
 
 const Smurfs = () => {
+  const [smurf, setSmurf] = useState({
+    name: "",
+    height: "",
+    age: "",
+    id: Date.now(),
+  });
   const { smurfs, error, isFetching } = useSelector((state) => state);
   const dispatch = useDispatch();
 
   useEffect(() => {
     dispatch(getSmurfs());
-  }, []);
+  }, [smurfs]);
+
   console.log(smurfs);
+  const handleChange = (e) => {
+    e.preventDefault();
+    const newSmurf = { ...smurf, [e.target.name]: e.target.value };
+    setSmurf(newSmurf);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    dispatch(addSmurf(smurf));
+  };
+
   return (
     <div>
       <h1>Here are the Smurfs</h1>
       {smurfs.map((smurf) => (
         <Smurf key={smurf.id} smurf={smurf} />
       ))}
+      <h1>Or, add a Smurf to the Smurf Village</h1>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          id="name"
+          name="name"
+          placeholder="Name"
+          value={smurf.name}
+          onChange={handleChange}
+        ></input>
+        <input
+          type="text"
+          id="height"
+          name="height"
+          placeholder="Height"
+          value={smurf.height}
+          onChange={handleChange}
+        ></input>
+        <input
+          type="text"
+          id="age"
+          name="age"
+          placeholder="Age"
+          value={smurf.age}
+          onChange={handleChange}
+        ></input>
+        <button type="submit">Submit</button>
+      </form>
     </div>
   );
 };
